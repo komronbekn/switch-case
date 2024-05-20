9f4d14f68c59ea34348898c5e6f9eb0b418cd9ae
komronbekn <komronfront@gmail.com>
2024-05-20 14:16:05 +0500

    by komronbek

---
+++ main.js
@@ -0,0 +1,312 @@
+// let month = prompt('Oy kiriting')
+// switch (month) {
+//     case 'dekabr': console.log('Qish'); break
+//     case 'yanvar': console.log('Qish'); break   
+//     case 'fevral': console.log('Qish'); break
+//     case 'sentabr': console.log('kuz'); break
+//     case 'oktabr': console.log('kuz'); break
+//     case 'noyabr': console.log('kuz'); break
+//     case 'iyun': console.log('Yoz'); break
+//     case 'iyul': console.log('Yoz'); break
+//     case 'avgust': console.log('Yoz'); break
+//     case 'mart': console.log('bahor'); break
+//     case 'aprel': console.log('bahor'); break
+//     case 'may': console.log('bahor'); break
+// }
+
+// let min = +prompt('Minut kiriting')
+// let sutka = Math.floor  (min / 1440) 
+// let soat = Math.round((min % 1440) / 60)
+// let minut = Math.round((min % 1440) % 60 )
+// console.log(`sutka ${sutka}, soat${soat} minut${minut}`)
+// console.log(`${sutka} sutka`);
+
+
+let Hafta = +prompt("Hafta kunini sonini kiriting")
+switch(Hafta){
+    case 1:alert('Dushanba'); break
+    case 2 :alert('Seshanba'); break
+    case 3 :alert('chorshanba'); break
+    case 4:alert("Payshanba"); break
+    case 5 : alert("Juma"); break
+    case 6:alert('Shanba'); break
+    case 7 : alert('Yakshanba'); break
+    default:alert('Bunday kun yoq')
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
